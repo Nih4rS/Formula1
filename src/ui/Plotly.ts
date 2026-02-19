9d53f7b337c43aa81b5915e83f516d6335e326ff
Nih4rS <138818306+Nih4rS@users.noreply.github.com>
2026-02-19 13:21:10 +0530

    new update

--- src/ui/Plotly.ts
+++
@@ -1,7 +0,0 @@
-import createPlotlyComponent from 'react-plotly.js/factory'
-// @ts-expect-error no types for dist-min
-import Plotly from 'plotly.js-dist-min'
-
-const Plot = createPlotlyComponent(Plotly)
-export default Plot
-
